6ea818a5aa27d296633bd553d4a9041556552d56
sanchick02 <sanchick02@users.noreply.github.com>
2024-03-06 12:49:31 +0800

    Dataset and Data Cleaning

---
+++ DM.R
@@ -0,0 +1,10 @@
+# Import Libraries
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(readr)
+library(ggpubr)
+library(ggplot2)
+
+# Import Dataset
+data <- read.csv("~/DMAssignment/ObesityDataCleaned.csv")
